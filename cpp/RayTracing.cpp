45891bb699239c66534af30fc68a657225460483
Jørgen Dokken <jsd55@cam.ac.uk>
2022-05-12 10:25:07 +0000

    3D prototype created

--- cpp/RayTracing.cpp
+++ cpp/RayTracing.cpp
@@ -6,6 +6,7 @@
 // SPDX-License-Identifier:    MIT
 
 #include "RayTracing.h"
+#include <dolfinx/common/math.h>
 #include <xtensor/xadapt.hpp>
 #include <xtensor/xio.hpp>
 #include <xtensor/xslice.hpp>
@@ -13,16 +14,20 @@
 
 namespace
 {
+
 /// Get function that parameterizes a facet of a given cell
 /// @param[in] cell_type The cell type
 /// @param[in] facet_index The facet index (local to cell)
 /// @returns Function that computes the coordinate parameterization of the local
 /// facet on the reference cell.
-std::function<std::array<double, 3>(std::array<double, 2>)>
+std::function<xt::xtensor_fixed<double, xt::xshape<1, 3>>(
+    xt::xtensor_fixed<double, xt::xshape<2>>)>
 get_3D_parameterization(dolfinx::mesh::CellType cell_type, int facet_index)
 {
 
-  std::function<std::array<double, 3>(std::array<double, 2>)> func;
+  std::function<xt::xtensor_fixed<double, xt::xshape<1, 3>>(
+      xt::xtensor_fixed<double, xt::xshape<2>>)>
+      func;
   const int tdim = dolfinx::mesh::cell_dim(cell_type);
   const int num_facets = dolfinx::mesh::cell_num_entities(cell_type, tdim - 1);
   if (facet_index >= num_facets)
@@ -34,64 +39,74 @@ get_3D_parameterization(dolfinx::mesh::CellType cell_type, int facet_index)
   case dolfinx::mesh::CellType::tetrahedron:
     if (facet_index == 0)
     {
-      func = [](std::array<double, 2> xi) -> std::array<double, 3> {
-        return {xi[0], xi[1], 1 - xi[0] - xi[1]};
+      func = [](xt::xtensor_fixed<double, xt::xshape<2>> xi)
+          -> xt::xtensor_fixed<double, xt::xshape<1, 3>> {
+        return {{xi[0], xi[1], 1 - xi[0] - xi[1]}};
       };
     }
     else if (facet_index == 1)
     {
-      func = [](std::array<double, 2> xi) -> std::array<double, 3> {
-        return {0, xi[0], xi[1]};
+      func = [](xt::xtensor_fixed<double, xt::xshape<2>> xi)
+          -> xt::xtensor_fixed<double, xt::xshape<1, 3>> {
+        return {{0, xi[0], xi[1]}};
       };
     }
     else if (facet_index == 2)
     {
-      func = [](std::array<double, 2> xi) -> std::array<double, 3> {
-        return {xi[0], 0, xi[1]};
+      func = [](xt::xtensor_fixed<double, xt::xshape<2>> xi)
+          -> xt::xtensor_fixed<double, xt::xshape<1, 3>> {
+        return {{xi[0], 0, xi[1]}};
       };
     }
     else if (facet_index == 3)
     {
-      func = [](std::array<double, 2> xi) -> std::array<double, 3> {
-        return {xi[0], xi[1], 0};
+      func = [](xt::xtensor_fixed<double, xt::xshape<2>> xi)
+          -> xt::xtensor_fixed<double, xt::xshape<1, 3>> {
+        return {{xi[0], xi[1], 0}};
       };
     }
     break;
   case dolfinx::mesh::CellType::hexahedron:
     if (facet_index == 0)
     {
-      func = [](std::array<double, 2> xi) -> std::array<double, 3> {
-        return {xi[0], xi[1], 0};
+      func = [](xt::xtensor_fixed<double, xt::xshape<2>> xi)
+          -> xt::xtensor_fixed<double, xt::xshape<1, 3>> {
+        return {{xi[0], xi[1], 0}};
       };
     }
     else if (facet_index == 1)
     {
-      func = [](std::array<double, 2> xi) -> std::array<double, 3> {
-        return {xi[0], 0, xi[1]};
+      func = [](xt::xtensor_fixed<double, xt::xshape<2>> xi)
+          -> xt::xtensor_fixed<double, xt::xshape<1, 3>> {
+        return {{xi[0], 0, xi[1]}};
       };
     }
     else if (facet_index == 2)
     {
-      func = [](std::array<double, 2> xi) -> std::array<double, 3> {
-        return {0, xi[0], xi[1]};
+      func = [](xt::xtensor_fixed<double, xt::xshape<2>> xi)
+          -> xt::xtensor_fixed<double, xt::xshape<1, 3>> {
+        return {{0, xi[0], xi[1]}};
       };
     }
     else if (facet_index == 3)
     {
-      func = [](std::array<double, 2> xi) -> std::array<double, 3> {
-        return {1, xi[0], xi[1]};
+      func = [](xt::xtensor_fixed<double, xt::xshape<2>> xi)
+          -> xt::xtensor_fixed<double, xt::xshape<1, 3>> {
+        return {{1, xi[0], xi[1]}};
       };
     }
     else if (facet_index == 4)
     {
-      func = [](std::array<double, 2> xi) -> std::array<double, 3> {
-        return {xi[0], 1, xi[1]};
+      func = [](xt::xtensor_fixed<double, xt::xshape<2>> xi)
+          -> xt::xtensor_fixed<double, xt::xshape<1, 3>> {
+        return {{xi[0], 1, xi[1]}};
       };
     }
     else if (facet_index == 5)
     {
-      func = [](std::array<double, 2> xi) -> std::array<double, 3> {
-        return {xi[0], xi[1], 1};
+      func = [](xt::xtensor_fixed<double, xt::xshape<2>> xi)
+          -> xt::xtensor_fixed<double, xt::xshape<1, 3>> {
+        return {{xi[0], xi[1], 1}};
       };
     }
     break;
@@ -106,7 +121,7 @@ get_3D_parameterization(dolfinx::mesh::CellType cell_type, int facet_index)
 /// @param[in] cell_type The cell type
 /// @param[in] facet_index The facet index (local to cell)
 /// @returns The Jacobian of the parameterization
-std::array<std::array<double, 3>, 2>
+xt::xtensor_fixed<double, xt::xshape<3, 2>>
 get_parameterization_jacobian(dolfinx::mesh::CellType cell_type,
                               int facet_index)
 {
@@ -121,39 +136,40 @@ get_parameterization_jacobian(dolfinx::mesh::CellType cell_type,
   {
   case dolfinx::mesh::CellType::tetrahedron:
     if (facet_index == 0)
-      return {{{1, 0, -1}, {0, 1, -1}}};
+      return {{1, 0}, {0, 1}, {-1, -1}};
     else if (facet_index == 1)
-      return {{{0, 1, 0}, {0, 0, 1}}};
+      return {{0, 0}, {1, 0}, {0, 1}};
+
     else if (facet_index == 2)
-      return {{{1, 0, 0}, {0, 0, 1}}};
+      return {{1, 0}, {0, 0}, {0, 1}};
     else if (facet_index == 3)
-      return {{{1, 0, 0}, {0, 1, 0}}};
+      return {{1, 0}, {0, 1}, {0, 0}};
     break;
   case dolfinx::mesh::CellType::hexahedron:
     if ((facet_index == 0) or (facet_index == 5))
-      return {{{1, 0, 0}, {0, 1, 0}}};
+      return {{1, 0}, {0, 1}, {0, 0}};
     else if ((facet_index == 1) or (facet_index == 4))
-      return {{{1, 0, 0}, {0, 0, 1}}};
+      return {{1, 0}, {0, 0}, {0, 1}};
     else if ((facet_index == 2) or (facet_index) == 3)
-      return {{{0, 1, 0}, {0, 0, 1}}};
+      return {{0, 0}, {1, 0}, {0, 1}};
     break;
   default:
     throw std::invalid_argument("Unsupported cell type");
     break;
   }
-  std::array<std::array<double, 3>, 2> output;
+  xt::xtensor_fixed<double, xt::xshape<3, 2>> output;
   return output;
 }
 
 } // namespace
 
-std::tuple<int, std::array<double, 3>, std::array<double, 3>>
-dolfinx_contact::compute_3D_ray(const dolfinx::mesh::Mesh& mesh,
-                                const std::array<double, 3>& point,
-                                const std::array<double, 3>& t1,
-                                const std::array<double, 3>& t2, int cell,
-                                int facet_index, const int max_iter,
-                                const double tol)
+std::tuple<int, xt::xtensor_fixed<double, xt::xshape<3>>,
+           xt::xtensor_fixed<double, xt::xshape<3>>>
+dolfinx_contact::compute_3D_ray(
+    const dolfinx::mesh::Mesh& mesh,
+    const xt::xtensor_fixed<double, xt::xshape<3>>& point,
+    const xt::xtensor_fixed<double, xt::xshape<2, 3>>& tangents, int cell,
+    int facet_index, const int max_iter, const double tol)
 {
   int status = -1;
   dolfinx::mesh::CellType cell_type = mesh.topology().cell_type();
@@ -161,7 +177,7 @@ dolfinx_contact::compute_3D_ray(const dolfinx::mesh::Mesh& mesh,
 
   // Get parameterization function and jacobian
   auto xi = get_3D_parameterization(cell_type, facet_index);
-  std::array<std::array<double, 3>, 2> dxi
+  xt::xtensor_fixed<double, xt::xshape<3, 2>> dxi
       = get_parameterization_jacobian(cell_type, facet_index);
 
   const dolfinx::fem::CoordinateElement& cmap = mesh.geometry().cmap();
@@ -182,21 +198,29 @@ dolfinx_contact::compute_3D_ray(const dolfinx::mesh::Mesh& mesh,
     std::copy_n(std::next(x_g.begin(), 3 * x_dofs[j]), 3,
                 std::next(coordinate_dofs.begin(), j * 3));
   }
-
+  if ((gdim != tdim) or (gdim != 3))
+  {
+    throw std::invalid_argument(
+        "This raytracing algorithm is specialized for meshes with topological "
+        "and geometrical dimension 3");
+  }
+  std::cout << "cell coords " << coordinate_dofs << "\n";
   // Temporary variables
   xt::xtensor<double, 2> X_k({1, 3});
   xt::xtensor<double, 2> x_k({1, 3});
-  std::array<double, 2> xi_k = {0.5, 0.25};
-  std::array<double, 3> x;
-  std::array<double, 3> X;
-  xt::xtensor<double, 2> J({(std::size_t)gdim, (std::size_t)tdim});
+  xt::xtensor_fixed<double, xt::xshape<2>> xi_k = {0.5, 0.25};
+  xt::xtensor_fixed<double, xt::xshape<2>> dxi_k;
+
+  xt::xtensor_fixed<double, xt::xshape<3, 3>> J;
   xt::xtensor<double, 2> dphi({(std::size_t)tdim, num_dofs_g});
-  std::array<double, 2> G_k;
+  xt::xtensor_fixed<double, xt::xshape<3, 2>> dGk_tmp;
+  xt::xtensor_fixed<double, xt::xshape<2, 2>> dGk;
+  xt::xtensor_fixed<double, xt::xshape<2, 2>> dGk_inv;
+  xt::xtensor_fixed<double, xt::xshape<2>> Gk;
   for (int k = 0; k < max_iter; ++k)
   {
     // Evaluate reference coordinate at current iteration
-    X = xi(xi_k);
-    std::copy(X.cbegin(), X.cend(), X_k.begin());
+    X_k = xi(xi_k);
 
     // Tabulate coordinate element basis function
     cmap.tabulate(1, X_k, basis_values);
@@ -207,33 +231,79 @@ dolfinx_contact::compute_3D_ray(const dolfinx::mesh::Mesh& mesh,
     dphi = xt::view(basis_values, xt::xrange(1, tdim + 1), 0, xt::all(), 0);
 
     // Compute Jacobian
+    std::fill(J.begin(), J.end(), 0);
     cmap.compute_jacobian(dphi, coordinate_dofs, J);
 
     // Compute residual at current iteration
-    std::fill(G_k.begin(), G_k.end(), 0);
+    std::fill(Gk.begin(), Gk.end(), 0);
     for (std::size_t i = 0; i < 3; ++i)
     {
-      G_k[0] += (x_k(0, i) - point[i]) * t1[i];
-      G_k[1] += (x_k(0, i) - point[i]) * t2[i];
+      Gk[0] += (x_k(0, i) - point[i]) * tangents(0, i);
+      Gk[1] += (x_k(0, i) - point[i]) * tangents(1, i);
     }
 
     // Check for convergence in first iteration
-    if ((k == 0) and (std::abs(G_k[0]) < tol) and (std::abs(G_k[1]) < tol))
+    if ((k == 0) and (std::abs(Gk[0]) < tol) and (std::abs(Gk[1]) < tol))
       break;
 
-    /// Compute dG_k/dxi
+    /// Compute dGk/dxi
+    std::fill(dGk_tmp.begin(), dGk_tmp.end(), 0);
+    dolfinx::math::dot(J, dxi, dGk_tmp);
+    std::fill(dGk.begin(), dGk.end(), 0);
 
-    // Invert dG_k/dxi
+    for (std::size_t i = 0; i < 2; ++i)
+      for (std::size_t j = 0; j < 2; ++j)
+        for (std::size_t l = 0; l < 3; ++l)
+          dGk(i, j) += dGk_tmp(l, j) * tangents(i, l);
 
-    // Compute dxi and check for convergence
+    // Invert dGk/dxi
+    double det_dGk = dolfinx::math::det(dGk);
+    if (std::abs(det_dGk) < tol)
+    {
+      status = -2;
+      break;
+    }
+    dolfinx::math::inv(dGk, dGk_inv);
+
+    // Compute dxi
+    std::fill(dxi_k.begin(), dxi_k.end(), 0);
+    for (std::size_t i = 0; i < 2; ++i)
+      for (std::size_t j = 0; j < 2; ++j)
+        dxi_k[i] += dGk_inv(i, j) * Gk[j];
+
+    // Check for convergence
+    if ((dxi_k[0] * dxi_k[0] + dxi_k[1] * dxi_k[1]) < tol * tol)
+    {
+      status = 1;
+      break;
+    }
 
     // Update xi
+    std::transform(xi_k.cbegin(), xi_k.cend(), dxi_k.cbegin(), xi_k.begin(),
+                   [](auto x, auto y) { return x - y; });
+  }
+  // Check if converged  parameters are valid
+  switch (cell_type)
+  {
+  case dolfinx::mesh::CellType::tetrahedron:
+    if ((xi_k[0] < 0) or (xi_k[0] > 1) or (xi_k[1] < 0)
+        or (xi_k[1] > 1 - xi_k[0]))
+    {
+      status = -3;
+    }
+    break;
+  case dolfinx::mesh::CellType::hexahedron:
+    if ((xi_k[0] < 0) or (xi_k[0] > 1) or (xi_k[1] < 0) or (xi_k[1] > 1))
+    {
+      status = -3;
+    }
+    break;
+  default:
+    throw std::invalid_argument("Invalid cell type");
   }
 
-  // Check if converged  parameters are valid
-  std::cout << X_k << " " << x_k << "\n";
-  std::cout << xt::adapt(dxi[0]) << "\n;";
-  std::tuple<int, std::array<double, 3>, std::array<double, 3>> output
-      = std::make_tuple(status, x, X);
+  std::tuple<int, xt::xtensor_fixed<double, xt::xshape<3>>,
+             xt::xtensor_fixed<double, xt::xshape<3>>>
+      output = std::make_tuple(status, x_k, X_k);
   return output;
 };
--- cpp/RayTracing.h
+++ cpp/RayTracing.h
@@ -17,14 +17,15 @@ namespace dolfinx_contact
 /// The implementation solves dot(\Phi(\xi, \eta)-p, t_i)=0, i=1,2
 /// where \Phi(\xi,\eta) is the parameterized mapping from the reference facet
 /// to the physical facet, p the point of origin of the ray, and t_1, t_2 the
-/// two tangents defining the ray.
+/// two tangents defining the ray. For more details, see
+/// DOI: 10.1016/j.compstruc.2015.02.027 (eq 14).
 ///
 /// @note The problem is solved using Newton's method
 ///
 /// @param[in] mesh The mesh
 /// @param[in] point The point of origin for the ray
-/// @param[in] t1 The first tangent of the ray
-/// @param[in] t2 The second tangent of the ray
+/// @param[in] tangents The tangents of the ray. Each row corresponds to a
+/// tangent.
 /// @param[in] cell The cell index (local to process)
 /// @param[in] facet_index The facet index (local to cell)
 /// @param[in] max_iter The maximum number of iterations to use for Newton's
@@ -35,9 +36,12 @@ namespace dolfinx_contact
 /// @note The convergence status is >0 if converging, -1 if the facet is if the
 /// maximum number of iterations are reached, -2 if the facet is parallel with
 /// the tangent, -3 if the Newton solver finds a solution outside the element.
-std::tuple<int, std::array<double, 3>, std::array<double, 3>> compute_3D_ray(
-    const dolfinx::mesh::Mesh& mesh, const std::array<double, 3>& point,
-    const std::array<double, 3>& t1, const std::array<double, 3>& t2, int cell,
-    int facet_index, const int max_iter = 25, const double tol = 1e-8);
+std::tuple<int, xt::xtensor_fixed<double, xt::xshape<3>>,
+           xt::xtensor_fixed<double, xt::xshape<3>>>
+compute_3D_ray(const dolfinx::mesh::Mesh& mesh,
+               const xt::xtensor_fixed<double, xt::xshape<3>>& point,
+               const xt::xtensor_fixed<double, xt::xshape<2, 3>>& tangents,
+               int cell, int facet_index, const int max_iter = 25,
+               const double tol = 1e-8);
 
 } // namespace dolfinx_contact
