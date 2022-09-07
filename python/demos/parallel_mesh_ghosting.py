31d213d72e956307a0f6fbe2d7c481b14878e462
Chris Richardson <chris@bpi.cam.ac.uk>
2022-09-07 09:59:03 +0100

    Still not quite right

---
+++ python/demos/parallel_mesh_ghosting.py
@@ -0,0 +1,46 @@
+from mpi4py import MPI
+from dolfinx.io import XDMFFile
+from dolfinx.mesh import create_mesh
+import dolfinx
+import numpy as np
+
+xdmf = XDMFFile(MPI.COMM_WORLD, 'box_2D.xdmf', 'r')
+mesh = xdmf.read_mesh()
+tdim = 2
+mesh.topology.create_entities(tdim - 1)
+marker = xdmf.read_meshtags(mesh, 'facet_marker')
+
+# Get cells attached to marked facets
+mesh.topology.create_connectivity(tdim - 1, tdim)
+fc = mesh.topology.connectivity(tdim - 1, tdim)
+cells = [fc.links(f)[0] for f in marker.indices]
+ncells = mesh.topology.index_map(tdim).size_local
+
+def partitioner(comm, n, m, topo):
+    rank = comm.Get_rank()
+    other_ranks = [i for i in range(comm.Get_size()) if i != rank]
+
+    dests = []
+    offsets = [0]
+    for c in range(ncells):
+        dests.append(rank) # Keep ownership
+        if c in cells:
+            dests.extend(other_ranks) # Ghost to other processes
+        offsets.append(len(dests))
+    return dolfinx.cpp.graph.AdjacencyList_int32(dests, offsets)
+
+# Convert topology to global indexing, and restrict to non-ghost cells
+topo = mesh.topology.connectivity(tdim, 0).array
+topo = mesh.topology.index_map(0).local_to_global(topo).reshape((-1, 3))
+topo = topo[:ncells,:]
+
+# Cut off any ghost vertices
+num_vertices = mesh.topology.index_map(0).size_local
+x = mesh.geometry.x[:, :num_vertices]
+domain = mesh.ufl_domain()
+new_mesh = create_mesh(mesh.comm, topo, x, domain, partitioner)
+
+print(new_mesh.topology.index_map(tdim).num_ghosts)
+
+new_xdmf = XDMFFile(MPI.COMM_WORLD, "output.xdmf", "w")
+new_xdmf.write_mesh(new_mesh)
