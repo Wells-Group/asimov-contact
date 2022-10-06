ac372704bfe07cf920332d30037f9e995c0bd0b5
Chris Richardson <chris@bpi.cam.ac.uk>
2022-10-06 17:54:10 +0100

    First attempt at meshtag transfer

--- python/demos/parallel_mesh_ghosting.py
+++ python/demos/parallel_mesh_ghosting.py
@@ -1,6 +1,6 @@
 from mpi4py import MPI
 from dolfinx.io import XDMFFile
-from dolfinx.mesh import create_mesh
+from dolfinx.mesh import create_mesh, meshtags
 import dolfinx
 import numpy as np
 
@@ -13,8 +13,22 @@ marker = xdmf.read_meshtags(mesh, 'facet_marker')
 # Get cells attached to marked facets
 mesh.topology.create_connectivity(tdim - 1, tdim)
 fc = mesh.topology.connectivity(tdim - 1, tdim)
-cells = [fc.links(f)[0] for f in marker.indices]
+fv = mesh.topology.connectivity(tdim - 1, 0)
+cells_to_ghost = [fc.links(f)[0] for f in marker.indices]
 ncells = mesh.topology.index_map(tdim).size_local
+
+# Convert marked facets to list of (global) vertices for each facet
+fv_indices = [sorted(mesh.topology.index_map(0).local_to_global(fv.links(f))) for f in marker.indices]
+global_markers = []
+for v in fv_indices:
+    global_markers += v
+
+# Copy facets and markers to all processes
+all_indices = mesh.comm.allgather(global_markers)
+all_indices = np.array(sum(all_indices, [])).reshape(-1, tdim)
+all_values = np.array(sum(mesh.comm.allgather(list(marker.values)), []))
+print(all_indices, all_values)
+
 
 def partitioner(comm, n, m, topo):
     rank = comm.Get_rank()
@@ -24,15 +38,16 @@ def partitioner(comm, n, m, topo):
     offsets = [0]
     for c in range(ncells):
         dests.append(rank)
-        if c in cells:
-            dests.extend(other_ranks) # Ghost to other processes
+        if c in cells_to_ghost:
+            dests.extend(other_ranks)  # Ghost to other processes
         offsets.append(len(dests))
     return dolfinx.cpp.graph.AdjacencyList_int32(dests, offsets)
 
+
 # Convert topology to global indexing, and restrict to non-ghost cells
 topo = mesh.topology.connectivity(tdim, 0).array
-topo = mesh.topology.index_map(0).local_to_global(topo).reshape((-1, 3))
-topo = topo[:ncells,:]
+topo = mesh.topology.index_map(0).local_to_global(topo).reshape((-1, tdim + 1))
+topo = topo[:ncells, :]
 
 # Cut off any ghost vertices
 num_vertices = mesh.topology.index_map(0).size_local
@@ -40,7 +55,26 @@ x = mesh.geometry.x[:num_vertices, :]
 domain = mesh.ufl_domain()
 new_mesh = create_mesh(mesh.comm, topo, x, domain, partitioner)
 
-print(new_mesh.topology.index_map(tdim).num_ghosts)
+# Recreate facet markers
+new_mesh.topology.create_entities(tdim - 1)
+# Create a list of all facet-vertices (global index)
+fv = new_mesh.topology.connectivity(tdim - 1, 0)
+fv_indices = np.array([sorted(new_mesh.topology.index_map(0).local_to_global(fv.links(f)))
+                      for f in range(fv.num_nodes)])
+# Search for marked facets in list of all facets
+new_marker_indices = []
+new_marker_values = []
+for idx, val in zip(all_indices, all_values):
+    f = np.nonzero(np.all(fv_indices == idx, axis=1))[0]
+    if len(f) > 0:
+        assert len(f) == 1
+        f = f[0]
+        new_marker_indices += [f]
+        new_marker_values += [val]
+
+new_meshtag = meshtags(mesh, tdim - 1, np.array(new_marker_indices, dtype=np.int32), new_marker_values)
+print(new_marker_indices, new_marker_values)
 
 new_xdmf = XDMFFile(MPI.COMM_WORLD, "output.xdmf", "w")
 new_xdmf.write_mesh(new_mesh)
+new_xdmf.write_meshtags(new_meshtag)
