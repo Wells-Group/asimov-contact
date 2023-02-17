35781efdeed4365a17afd7496a291146bde2ee3e
Chris Richardson <chris@bpi.cam.ac.uk>
2023-02-17 15:19:10 +0000

    More cpp mesh parallel (#127)
    * Still not quite right
    
    * Bug fix
    
    * Generalise tdim
    
    * First attempt at meshtag transfer
    
    * Fix using original vertex indexing
    
    * Fix mesh
    
    * Fix by applying another mapping
    
    * Slight rewrite
    
    * Add routine for point-cloud search
    
    * Work towards processing on zero
    
    * Use neighbors for ghosting
    
    * flake8
    
    * Work on sending markers
    
    * flake8
    
    * Use matplotlib to visualise
    
    * Bug fix and plot facets
    
    * More graphics
    
    * Neaten up a bit
    
    * Updates
    
    * Move more into python package
    
    * Add debug output
    
    * Updates for parallel ghosting
    
    * flake8 fixes
    
    * Move point cloud search to cpp
    
    * Some debug
    
    * Some protection for zero size arrays
    
    * Work on conversion to C++
    
    * Tidy up in ghost cell computation
    
    * Remove unused python code
    
    * Add copyright headers
    
    * Debug divide by zero
    
    * increase search radius

---
+++ cpp/parallel_mesh_ghosting.h
@@ -0,0 +1,17 @@
+// Copyright (C) 2023 Chris N. Richardson
+//
+// This file is part of DOLFINx_CONTACT
+//
+// SPDX-License-Identifier:    MIT
+
+#include <dolfinx/graph/AdjacencyList.h>
+#include <dolfinx/mesh/Mesh.h>
+#include <span>
+
+namespace dolfinx_contact
+{
+  /// Compute destinations
+dolfinx::graph::AdjacencyList<std::int32_t>
+compute_ghost_cell_destinations(const dolfinx::mesh::Mesh& mesh,
+                                std::span<const std::int32_t> marker_subset, double R);
+}
