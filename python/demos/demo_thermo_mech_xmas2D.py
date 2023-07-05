e90f6af54747274b0526bddff4c48a0bd956e563
Sarah Roggendorf <33656497+SarahRo@users.noreply.github.com>
2023-07-05 15:15:51 +0100

    fix failing CI (#144)
    * fix failing CI
    
    * another fix

--- python/demos/demo_thermo_mech_xmas2D.py
+++ python/demos/demo_thermo_mech_xmas2D.py
@@ -5,6 +5,7 @@ import dolfinx.fem as _fem
 import numpy as np
 import ufl
 from dolfinx import io, log
+from dolfinx.fem.petsc import LinearProblem
 from dolfinx.graph import create_adjacencylist
 from dolfinx.io import XDMFFile
 from dolfinx_contact.helpers import (epsilon, lame_parameters, sigma_func,
@@ -46,13 +47,13 @@ therm = (q - T0) * r * dx + kdt * ufl.inner(ufl.grad(tau * q + (1 - tau) * T0), 
 
 a_therm, L_therm = ufl.lhs(therm), ufl.rhs(therm)
 
-T0.x.set(0.0)
+T0.x.array[:] = 0
 dofs = _fem.locate_dofs_topological(Q, entity_dim=tdim - 1, entities=facet_marker.find(3))
 Tbc = _fem.dirichletbc(value=_PETSc.ScalarType((1.0)), dofs=dofs, V=Q)
 dofs2 = _fem.locate_dofs_topological(Q, entity_dim=tdim - 1, entities=facet_marker.find(4))
 Tbc2 = _fem.dirichletbc(value=_PETSc.ScalarType((0.0)), dofs=dofs2, V=Q)
-Tproblem = _fem.petsc.LinearProblem(a_therm, L_therm, bcs=[Tbc, Tbc2], petsc_options={
-                                    "ksp_type": "preonly", "pc_type": "lu"}, u=T0)
+Tproblem = LinearProblem(a_therm, L_therm, bcs=[Tbc, Tbc2], petsc_options={
+    "ksp_type": "preonly", "pc_type": "lu"}, u=T0)
 
 
 # Elasticity problem
