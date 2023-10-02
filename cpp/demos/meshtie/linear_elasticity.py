885342ffdcde6a468ea5ee022e061b98d22f0c20
Sarah Roggendorf <33656497+SarahRo@users.noreply.github.com>
2023-10-02 17:24:44 +0100

    Changes in line with updates to basix (#154)
    * fix ufl file
    
    * format ufl fil
    
    * fix block demo

--- cpp/demos/meshtie/linear_elasticity.py
+++ cpp/demos/meshtie/linear_elasticity.py
@@ -8,7 +8,7 @@
 
 from basix.ufl import element
 from ufl import (Coefficient, ds, dx, FunctionSpace, grad, Identity,
-                  inner, Mesh, sym, TestFunction, tr, TrialFunction)
+                 inner, Mesh, sym, TestFunction, tr, TrialFunction)
 
 
 # tags for boundaries (see mesh file)
@@ -16,17 +16,18 @@ neumann_bdy = 7
 contact_bdy_1 = 6  # top contact interface
 contact_bdy_2 = 13  # bottom contact interface
 
-e = element("Lagrange", "tetrahedron", 1, rank=1)
+e = element("Lagrange", "tetrahedron", 1, shape=(3,))
 e0 = element("DG", "tetrahedron", 0)
-coord_element = element("Lagrange", "tetrahedron", 1, rank=1)
+coord_element = element("Lagrange", "tetrahedron", 1, shape=(3,))
 mesh = Mesh(coord_element)
 V = FunctionSpace(mesh, e)
 V0 = FunctionSpace(mesh, e0)
 v = TestFunction(V)
-w = TrialFunction(V) 
+w = TrialFunction(V)
 
 lmbda = Coefficient(V0)
 mu = Coefficient(V0)
+
 
 def epsilon(z):
     return sym(grad(z))
@@ -35,13 +36,14 @@ def epsilon(z):
 def sigma(z):
     return (2.0 * mu * epsilon(z) + lmbda * tr(epsilon(z)) * Identity(len(z)))
 
+
 # Bilinear form
 J = inner(sigma(w), epsilon(v)) * dx
 
 f = Coefficient(V)
 t = Coefficient(V)
 
-# Linear form 
+# Linear form
 F = inner(f, v) * dx
 F += inner(t, v) * ds(neumann_bdy)
 
